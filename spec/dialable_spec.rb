e6da28d6228eb9ac0dc727e1e176767df9f1eb90
Ben Hughes <ben@railsgarden.com>
2010-10-13 21:34:11 +0800

    Added a starter spec.

--- spec/dialable_spec.rb
+++ spec/dialable_spec.rb
@@ -1,7 +1,13 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe "dialable" do
-  it "should do nothing" do
-    true.should == true
+describe Dialable do
+  describe "with a full NANP number with extension" do
+    subject { Dialable::NANP.parse("+1(307)555-1212 ext 1234") }
+    
+    it("should extract the area code") { subject.areacode.should == "307" }
+    it("should extract the prefix") { subject.prefix.should == "555" }
+    it("should extract the line number") { subject.line.should == "1212" }
+    it("should extract the extension") { subject.extension.should == "1234" }
+    it("should determine the time zone") { subject.timezones.should == ["MST"] }
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
-$TESTING=true
-$:.push File.join(File.dirname(__FILE__), '..', 'lib')
+$LOAD_PATH.push File.join(File.dirname(__FILE__), '..', 'lib')
+
+require "rubygems"
+require "spec"
+require "active_support"
+require "dialable"
+
+Spec::Runner.configure do |config|
+  # config.mock_with :mocha
+end
